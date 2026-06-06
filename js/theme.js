0140ab702a565c1dfb09854c3f5965a29bdcec1e
Anda Popovici <anda.popovici@freeagent.com>
2026-06-06 22:24:35 +0100

    Finish off themes

--- js/theme.js
+++ js/theme.js
@@ -4,22 +4,21 @@ const initialTheme = savedTheme ? savedTheme : "auto";
 document.documentElement.setAttribute("data-theme", initialTheme);
 
 // set loaded theme button as aria-pressed
-const selectedButton = document.querySelector(`button[data-theme=${initialTheme}]`);
+const selectedButton = document.querySelector(`button[data-theme-value=${initialTheme}]`);
 if (selectedButton) {
   selectedButton.setAttribute("aria-pressed", "true");
 }
-console.log(selectedButton);
 
 const buttons = document.querySelectorAll("button[data-toggle-theme]");
 
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
-    const newTheme = this.dataset.theme;
+    const newTheme = this.dataset.themeValue;
     const previousTheme = localStorage.getItem("andaBlogTheme");
 
     // check if theme in allowed themes
     if (["auto", "light", "dark", "pride"].includes(newTheme) && newTheme !== previousTheme) {
-      const previousThemeButton = document.querySelector(`button[data-theme=${previousTheme}]`);
+      const previousThemeButton = document.querySelector(`button[data-theme-value=${previousTheme}]`);
       previousThemeButton.removeAttribute("aria-pressed");
       this.setAttribute("aria-pressed", "true");
       document.documentElement.setAttribute("data-theme", newTheme);
